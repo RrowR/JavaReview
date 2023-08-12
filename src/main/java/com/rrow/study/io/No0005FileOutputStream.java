ed40a39f3d76e7d8a4502b37bb42990a77558699
Memo <2415782224@qq.com>
2023-08-12 09:19:16 +0800

    FileOutputStream

---
+++ src/main/java/com/rrow/study/io/No0005FileOutputStream.java
@@ -0,0 +1,20 @@
+package com.rrow.study.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author: Rrow
+ * @date: 2023/8/12 9:16
+ * Description: FileOutputStream 用于写入诸如图像数据之类的原始字节的流
+ */
+public class No0005FileOutputStream {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("output.txt")) {
+            byte[] array = "JavaGuide".getBytes();
+            output.write(array);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
