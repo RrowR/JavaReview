3188a0428eb95d3065a3ed03ff9de894d5a1fe94
Memo <2415782224@qq.com>
2023-08-12 09:21:47 +0800

    BufferedOutputStream

---
+++ src/main/java/com/rrow/study/io/No0006BufferedOutputStream.java
@@ -0,0 +1,21 @@
+package com.rrow.study.io;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author: Rrow
+ * @date: 2023/8/12 9:20
+ * Description: BufferedOutputStream
+ */
+public class No0006BufferedOutputStream {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("bos.txt");
+        BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+        bos.write("Hello bos".getBytes());
+        bos.flush();
+        bos.close();
+    }
+}
