1c30444c5927a24a0657eb00dfd4026fd3e1231e
Memo <2415782224@qq.com>
2023-08-13 17:17:46 +0800

    更新编译文件

--- src/main/java/com/rrow/study/io/字节流/No0002BufferedInputStream.java
+++ src/main/java/com/rrow/study/io/字节流/No0002BufferedInputStream.java
@@ -11,11 +11,11 @@ import java.io.IOException;
  */
 public class No0002BufferedInputStream {
     public static void main(String[] args) throws IOException {
-        // 新建一个 BufferedInputStream 对象
-        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("input.txt"));
-        // 读取文件的内容并复制到 String 对象中
-        byte[] bytes = bufferedInputStream.readAllBytes();
-        String result = new String(bytes);
-        System.out.println(result);
+        // // 新建一个 BufferedInputStream 对象
+        // BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("input.txt"));
+        // // 读取文件的内容并复制到 String 对象中
+        // byte[] bytes = bufferedInputStream.readAllBytes();
+        // String result = new String(bytes);
+        // System.out.println(result);
     }
 }
