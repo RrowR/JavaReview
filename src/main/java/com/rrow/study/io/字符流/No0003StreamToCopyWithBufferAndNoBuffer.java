af0fdb74caf7c54658d5863c698d56f6e753cd15
Memo <2415782224@qq.com>
2023-08-13 14:11:22 +0800

    字节缓冲流记录消耗时间

--- src/main/java/com/rrow/study/io/字符流/No0003StreamToCopyWithBufferAndNoBuffer.java
+++ src/main/java/com/rrow/study/io/字符流/No0003StreamToCopyWithBufferAndNoBuffer.java
@@ -45,4 +45,47 @@ public class No0003StreamToCopyWithBufferAndNoBuffer {
         long end = System.currentTimeMillis();
         System.out.println("使用普通流复制PDF文件总耗时:" + (end - start) + " 毫秒");
     }
+
+    /**
+     * 104 ms
+     */
+    @Test
+    public void use_array_to_copy_mp4_with_buffer() {
+        long start = System.currentTimeMillis();
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("1.mp4"));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("1-bak.mp4"))) {
+            int len;
+            byte[] bytes = new byte[4 * 1024];
+            while ((len = bis.read(bytes)) != -1) {
+                bos.write(bytes, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        System.out.println("使用缓冲流复制PDF文件总耗时:" + (end - start) + " 毫秒");
+    }
+
+    /**
+     * 151 ms
+     */
+    @Test
+    public void use_array_to_copy_mp4_with_fileOutPutStream_noBuffer() {
+        // 记录开始时间
+        long start = System.currentTimeMillis();
+        try (FileInputStream fis = new FileInputStream("1.mp4");
+             FileOutputStream fos = new FileOutputStream("1-bak.mp4")) {
+            int len;
+            byte[] bytes = new byte[4 * 1024];
+            while ((len = fis.read(bytes)) != -1) {
+                fos.write(bytes, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 记录结束时间
+        long end = System.currentTimeMillis();
+        System.out.println("使用普通流复制PDF文件总耗时:" + (end - start) + " 毫秒");
+    }
 }
