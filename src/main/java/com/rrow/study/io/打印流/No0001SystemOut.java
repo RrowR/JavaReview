81482424b1fa5c9dd144d35e5ac9ca6fc2b6626e
Memo <2415782224@qq.com>
2023-08-13 16:27:25 +0800

    打印流

---
+++ src/main/java/com/rrow/study/io/打印流/No0001SystemOut.java
@@ -0,0 +1,12 @@
+package com.rrow.study.io.打印流;
+
+/**
+ * @author: Rrow
+ * @date: 2023/8/13 16:17
+ * Description:
+ */
+public class No0001SystemOut {
+    public static void main(String[] args) {
+        System.out.println("hello world !!!");
+    }
+}
