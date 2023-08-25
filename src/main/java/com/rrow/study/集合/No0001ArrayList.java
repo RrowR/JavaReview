0c326d2f72efaf441e1265092220ac6581d675c5
Memo <2415782224@qq.com>
2023-08-25 16:49:33 +0800

    ArrayList

---
+++ src/main/java/com/rrow/study/集合/No0001ArrayList.java
@@ -0,0 +1,15 @@
+package com.rrow.study.集合;
+
+import java.util.ArrayList;
+
+/**
+ * @author: Rrow
+ * @date: 2023/8/19 14:57
+ * Description: Collection
+ */
+public class No0001ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+
+    }
+}
