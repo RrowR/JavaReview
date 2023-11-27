09916441939cad6c2c7bbe6909f40bad11b45a6f
RrowR <2415782224@qq.com>
2023-11-27 16:14:55 +0800

    面试题,线程安全类

---
+++ src/main/java/com/rrow/study/collection/CollectionStructures.java
@@ -0,0 +1,17 @@
+package com.rrow.study.collection;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author: Rrow
+ * @date: 2023/8/14 13:32
+ * Description:
+ */
+public class CollectionStructures {
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+        Set<Object> objects1 = new TreeSet<>();
+    }
+}
